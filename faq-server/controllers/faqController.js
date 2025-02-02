0bcfcab1bcfd7328fb74d38dd6154dec06660770
Mayuresh Khankale <khankalem.4275@gmail.com>
2025-02-02 23:36:56 +0530

    Update faqController.js for efficient cache retrieval

--- faq-server/controllers/faqController.js
+++ faq-server/controllers/faqController.js
@@ -86,6 +86,8 @@ const addFaq = async (req, res) => {
     const newFaq = new Faq({ question, answer, translations });
     await newFaq.save();
 
+    redis.flushdb().then(() => console.log("Cache cleared"));
+
     res.status(201).json(newFaq);
   } catch (err) {
     res.status(500).json({ message: "Server error" });
